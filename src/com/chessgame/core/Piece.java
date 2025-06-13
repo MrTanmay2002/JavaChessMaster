a31f228a2855fdc482f0560f54adef53ae5a5f9c
Tanmay Khobragade <102377152+MrTanmay2002@users.noreply.github.com>
2025-06-13 14:13:41 +0530

    Update and rename .java to Piece.java

---
+++ src/com/chessgame/core/Piece.java
@@ -0,0 +1,15 @@
+package com.chessgame.core;
+
+public abstract class Piece {
+    protected String color;
+
+    public Piece(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract boolean isValidMove(int x1, int y1, int x2, int y2);
+}
